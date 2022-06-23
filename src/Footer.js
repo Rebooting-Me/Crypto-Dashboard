0a94029434a676fac2ba36fb6a0eddde40e50d6b
Rebooting-Me <arsharma@ucsd.edu>
2022-06-22 18:36:27 -0700

    footer test

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,9 @@ import React from "react";
 
 export default function Footer () {
     return(
-  <div className="footer">
+  <footer className="footer">
     <p className="creators">Created By</p>
-  </div>
+    <p className="dev-names"><a href="https://github.com/DikshaVerma25" target="_blank">Diksha</a> & <a href="https://github.com/Rebooting-Me" target="_blank">Aditya</a> </p>
+  </footer>
   )
 }
