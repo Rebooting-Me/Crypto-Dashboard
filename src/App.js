4cdff55cae7e6a382e138639bdc9183b0e92e071
DikshaVerma25 <dikshaverma235@gmail.com>
2022-06-22 20:40:56 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Navbar from './Navbar';
+import Footer from "./Footer";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+  <Navbar />
+  <Footer/>
+  </div>
+  )
 }
 
 export default App;
