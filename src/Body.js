d48d6171021df18f8e2ac9dcce22496f079dc29d
Aditya Raj Sharma <96843549+Rebooting-Me@users.noreply.github.com>
2023-02-21 20:15:01 -0800

    Diksha (#1)
    * package-update
    
    * saas
    
    * bg-responsive
    
    ---------
    
    Co-authored-by: DikshaVerma25 <dikshaverma235@gmail.com>

--- src/Body.js
+++ src/Body.js
@@ -5,6 +5,7 @@ export default function Body() {
     return(
         <div className="main">
             <img className="background" src={bg} alt="Background" />
+            <img className="background1" src={bg} alt="Background" />
             <p>Journey Safe into the most trusted Blockchain Service on the Web!</p>
             <form action="/login">
                 <input type="text" id="email" name="email" placeholder="Enter your email here"></input>
